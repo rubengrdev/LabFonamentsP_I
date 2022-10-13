4a50677cd8a2ce98d0d1dacdad65832fda2b96e1
Ruben <rubengorodri@gmail.com>
2022-10-13 11:00:16 +0200

    update lab3

--- lab3/repte1_ruben.c
+++ lab3/repte1_ruben.c
@@ -7,16 +7,18 @@ int main(){
 
     printf("\nUna equacio de segon grau te la forma ax^2 + bx + c = 0");
     printf("\nEspecifica l'equacio introduint els valors de a, b i c");
-    printf("\nQuant val a?");
-    scanf("%d", &a);
-    printf("\nQuant val b?");
-    scanf("%d", &b);
-    printf("\nQuant val c?");
-    scanf("%d", &c);
+    do{
+        printf("\nQuant val a?");
+        scanf("%d", &a);
+        printf("\nQuant val b?");
+        scanf("%d", &b);
+        printf("\nQuant val c?");
+        scanf("%d", &c);
+        if(a == 0){
+            printf("\nL'equació no és de segón grau");
+        }
+    }while(a == 0);
 
-    if(a==0){
-        printf("\nL'equació no és de segón grau");
-    }else{
         if(((pow(b,2))-4*a*c) < 0){
             printf("\nL'equació no té solució");
         }else{
@@ -25,7 +27,6 @@ int main(){
             resultat2 = (-(b)-sqrt(traspas))/(2*a);
             printf("\nResultat en el cas positiu %.2f", resultat1);
             printf("\nResultat en el cas negatiu %.2f\n", resultat2);
-    }
     }
     return 0;
 }
--- lab3/repte3_ruben.c
+++ lab3/repte3_ruben.c
@@ -7,29 +7,30 @@ int main(){
 
     printf("\nUna equacio de segon grau te la forma ax^2 + bx + c = 0");
     printf("\nEspecifica l'equacio introduint els valors de a, b i c");
-    printf("\nQuant val a?");
-    scanf("%d", &a);
-    printf("\nQuant val b?");
-    scanf("%d", &b);
-    printf("\nQuant val c?");
-    scanf("%d", &c);
+    do{
+        printf("\nQuant val a?");
+        scanf("%d", &a);
+        printf("\nQuant val b?");
+        scanf("%d", &b);
+        printf("\nQuant val c?");
+        scanf("%d", &c);
+        if(a == 0){
+            printf("\nL'equació no és de segón grau");
+        }
+    }while(a == 0);
 
-    if(a==0){
-        printf("\nL'equació no és de segón grau");
-    }else{
         if(((pow(b,2))-4*a*c) < 0){
             printf("\nL'equació no té solució");
         }else{
             traspas = (pow(b,2)-4*a*c);
             resultat1 = (-(b)+sqrt(traspas))/(2*a);
             resultat2 = (-(b)-sqrt(traspas))/(2*a);
-            if(resultat1 != resultat2){
+             if(resultat1 != resultat2){
                 printf("\nResultat en el cas positiu %.2f", resultat1);
                 printf("\nResultat en el cas negatiu %.2f\n", resultat2);
             }else{
                 printf("\nEl resultat és %.2f", resultat1);
             }
-    }
     }
     return 0;
 }
