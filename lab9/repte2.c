660685e5d24b66a4b8e8e3ddca84751190e055f8
Ruben <rubengorodri@gmail.com>
2022-12-01 13:30:17 +0100

    solved errors, beta

--- lab9/repte2.c
+++ lab9/repte2.c
@@ -5,44 +5,36 @@
 #define MAX 100
 
 
-bool replace_string(char dada[]){
+int replace_string(char dada[]){
     int i, j = 0, k = 0;
     char newstring[MAX];
-    bool trigger = false, check = true;
-    /*No es una cadena vuida*/
-        printf("%s", dada);
+    //utilitzo una funció de C per poder agilitzar el procés strlen()
         for(i=0; i < (int)strlen(dada);i++){
             if((int)dada[i] == 32){
                 j = i + 1;
                 if(dada[j] >= 65 && dada[j] <= 90){
                    dada[j] = (int)dada[j] - 32;
-                   trigger = true;
                    k++;
                 }else if((int)dada[j] >= 97 && (int)dada[j] <= 122){
                     dada[j] = (int)dada[j] + 32;
-                    trigger = true;
                     k++;
                 }
             }
             newstring[i] = dada[j];
             j++;
         }
-        //en el cas de que 
-        if(!trigger){
-            check = false;
-        }
-        printf("\n%d",k);
-    return check;
+        //retorna el numero de modifiacions que necessita
+        return k;
 }
 int main (){
     char input[MAX];
-    bool status;
+    int mods;
     printf("\nInserta una cadena de text");
     //fgets per llegir cadena de text amb espais (string)
     fgets(input, MAX, stdin);
-    status = replace_string(input);
-    if(status){
-        printf("\nLa cadena s'ha de modificar");
+    mods = replace_string(input);
+    if(mods > 0){
+        printf("\nLa cadena s'ha de modificar %d vegades", mods);
     }else{
         printf("\nLa cadena NO s'ha de modificar");
     }
