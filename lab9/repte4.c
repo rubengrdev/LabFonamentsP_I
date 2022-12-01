5c7dc83898d45177c9104e0f8bbb3c0bb7ea90db
Ruben <rubengorodri@gmail.com>
2022-12-01 15:33:56 +0100

    last commit repte4

--- lab9/repte4.c
+++ lab9/repte4.c
@@ -9,7 +9,7 @@ bool comprova_string(char dada[]){
     int i, asciicode;
     bool trigger = false, check = true;
     /*No es una cadena vuida*/
-        while(dada[i] != '\0'){
+        for(i = 0; i < strlen(dada); i++){
             /*Comprvació letres en majuscula i minuscula i espai en blanc*/
             if(dada[i] >= 65 && dada[i] <= 90 || dada[i] >= 97 && dada[i] <= 122 || dada[i] == 32){
                 //dada correcta
@@ -28,7 +28,7 @@ bool comprova_string(char dada[]){
 
 int replace_string(char dada[]){
     int i, j = 0, k = 0;
-    char newstring[MAX];
+    char newstring[MAXSTR];
     //utilitzo una funció de C per poder agilitzar el procés strlen()
         for(i=0; i < (int)strlen(dada);i++){
             if((int)dada[i] == 32){
@@ -69,7 +69,7 @@ int main (){
     char inputphone[MAX];
     bool statusphone;
     int mods;
-    printf("\nInserta el teu nom, només pots utilitzar caràcters del tipus [a-z]-[A-Z]");
+    printf("\nInserta el teu nom complet, només pots utilitzar caràcters del tipus [a-z]-[A-Z]");
     do{
         //fgets per llegir cadena de text amb espais (string)
         fgets(input, MAXSTR, stdin);
@@ -77,6 +77,7 @@ int main (){
             printf("\nLa cadena és valida");
         }else{
             printf("\nLa cadena NO és valida");
+            printf("\nInserta el teu nom complet, només pots utilitzar caràcters del tipus [a-z]-[A-Z]");
         }
     }while(!comprova_string(input));
     printf("\nS'han fet %d conversions de lletres", replace_string(input));
@@ -84,9 +85,9 @@ int main (){
     do{
         fgets(inputphone, MAX, stdin);
         if(check_phone(inputphone)){
-            printf("\nLa cadena s'ha de modificar %d vegades", mods);
+            printf("\nEl telefon es correcte");
         }else{
-            printf("\nLa cadena NO s'ha de modificar");
+            printf("\nEl telefon es incorrecte\n");
         }
     }while(!check_phone(inputphone));
     return 0;
