515cadff5c84d9a8006bbaf57f4e356234e6f558
Ruben <rubengorodri@gmail.com>
2022-12-19 15:26:43 +0100

    to do prac1

--- pract1/xifrat.c
+++ pract1/xifrat.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
+#include <stdbool.h>
 
+int main() {
+    int i, j, k = 0, z = 0;
+    bool trigger = false;
+    char message[] = "CONTINUEM PLA2";
+    char encryptedMessage[] = "NFRFRIMIOFRIMFNARXAFRROTNXAA";
 
-
-int main(){
-    char matrix[6][6] = {
-        {'A', 'B', 'C', 'D', 'E', 'F'},
-        {'G', 'H', 'I', 'J', 'K', 'L'},
-        {'M', 'N', 'O', 'P', 'Q', 'R'},
-        {'S', 'T', 'U', 'V', 'W', 'X'},
-        {'Y', 'Z', ' ', '1', '2', '3'},
-        {'4', '5', '6', '7', '8', '9'}
-    };
-    char matrixorder[7][7] = {
-        {'X', 'I', 'F', 'R', 'A', 'T'},
+    char matrix[7][7] = {
+        {' ', 'X', 'I', 'F', 'R', 'A', 'T'},
         {'N', 'A', 'B', 'C', 'D', 'E', 'F'},
         {'O', 'G', 'H', 'I', 'J', 'K', 'L'},
         {'R', 'M', 'N', 'O', 'P', 'Q', 'R'},
@@ -21,7 +17,44 @@ int main(){
         {'L', '4', '5', '6', '7', '8', '9'}
     };
 
+    while(k < sizeof(message)){  
+        for(i = 0; i < 7; i++){
+            for(j = 0; j < 7; j++){
+                //la primera i fila [0] que referencia "i" i "j" és una base, no ha de se part de la clau de xifrat
+                if(i >= 1 && j >= 1){
+                    if(message[k] == matrix[i][j]){
+                        //printf("\n%c", matrix[i][j]);
+                        printf("%c%c", matrix[i][0], matrix[0][j]);
+                    }
+                }
+            }
+        }
+        k++;
+    }
 
+    int n = 0, m = 0, trigger= false;
+    while (z < sizeof(message)*2){
 
+        do{
+            if(){
+                trigger = true; 
+            }
+           n++; 
+        }while(!trigger);
+
+         for(i = 0; i < 7; i++){
+            for(j = 0; j < 7; j++){
+                //la primera i fila [0] que referencia "i" i "j" és una base, no ha de se part de la clau de xifrat
+                if(encryptedMessage[z] == matrix[0][j]){
+                    printf("\nword %d %d", i, j);
+                }
+                if(encryptedMessage[z] == matrix[i][0]){
+                    printf("\nword %d %d", j, i);
+                }
+            }
+        }
+        z++;
+    }
+   
     return 0;
 }
