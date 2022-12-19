92cbdffe047d6a5690cdcaee68eb64db4c6e3ae8
Ruben <rubengorodri@gmail.com>
2022-12-19 23:07:06 +0100

    create functions

--- pract1/xifrat.c
+++ pract1/xifrat.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int main() {
-    int i, j, k = 0, z = 0;
-    bool trigger = false;
-    char message[] = "CONTINUEM PLA2";
-    char encryptedMessage[] = "NFRFRIMIOFRIMFNARXAFRROTNXAA";
-
+char * construir_matriu_xifrat(){
     char matrix[7][7] = {
         {' ', 'X', 'I', 'F', 'R', 'A', 'T'},
         {'N', 'A', 'B', 'C', 'D', 'E', 'F'},
@@ -16,13 +11,20 @@ int main() {
         {'A', 'Y', 'Z', ' ', '1', '2', '3'},
         {'L', '4', '5', '6', '7', '8', '9'}
     };
+    return * matrix;
+}
 
-    while(k < sizeof(message)){  
+
+
+
+void xifrar_frase(char text[], char matrix[][7]){
+    int i, j, k = 0;
+    while(k < sizeof(text)){  
         for(i = 0; i < 7; i++){
             for(j = 0; j < 7; j++){
                 //la primera i fila [0] que referencia "i" i "j" és una base, no ha de se part de la clau de xifrat
                 if(i >= 1 && j >= 1){
-                    if(message[k] == matrix[i][j]){
+                    if(text[k] == matrix[i][j]){
                         //printf("\n%c", matrix[i][j]);
                         printf("%c%c", matrix[i][0], matrix[0][j]);
                     }
@@ -31,17 +33,19 @@ int main() {
         }
         k++;
     }
+}
 
-    printf("\n");
-    
-    while (z < sizeof(message)*2){
-        int n, m;
-        bool triggerRow = false, triggerColumn = false;
+void desxifrar_frase(char textXifrat[], char matrix[][7]){
+    int n, m, z = 0;
+    bool triggerRow, triggerColumn;
+    while (z < sizeof(textXifrat)){
+        triggerRow = false;
+        triggerColumn = false;
         n = 0;
         m = 0;
         /*obtenció de la posició n x m a l'array/taula de el caracter original*/
         do{
-            if(encryptedMessage[z] == matrix[n][0]){
+            if(textXifrat[z] == matrix[n][0]){
                 //l'objectiu es guardar el contador de "n" ja que té la posició n x m de la matriu
                 triggerRow = true;
                 z++;
@@ -51,7 +55,7 @@ int main() {
         }while(!triggerRow);
         
         do{
-            if(encryptedMessage[z] == matrix[0][m]){
+            if(textXifrat[z] == matrix[0][m]){
                 //l'objectiu es guardar el contador de "m" ja que té la posició n x m de la matriu
                 triggerColumn = true;
                 z++;
@@ -63,6 +67,18 @@ int main() {
         //demo decript
         printf("%c", matrix[n][m]);
     }
+}
+
+int main() {
+    int i, j, k = 0, z = 0;
+    bool trigger = false;
+    char message[] = "CONTINUEM PLA2";
+    char encryptedMessage[] = "NFRFRIMIOFRIMFNARXAFRROTNXAA";
+
+    
+    char * matrix = construir_matriu_xifrat();
+
+    
    
     return 0;
 }
