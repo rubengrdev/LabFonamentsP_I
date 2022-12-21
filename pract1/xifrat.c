bceb06459c0af4c3995d0d77af2c57e58779dcb1
Ruben <rubengorodri@gmail.com>
2022-12-21 12:02:43 +0100

    working encript and decript

--- pract1/xifrat.c
+++ pract1/xifrat.c
@@ -31,7 +31,6 @@ void construir_matriu_xifrat(char matrix[][7]){
 
 void xifrar_frase(char text[], char matrix[][7], char frase_xifrada[]){
     int i, j, k = 0, l = 0, o = 0;
-
     bool firtsValue = false;
     while(k < (obtindreMida(text))){  
         for(i = 0; i < 7; i++){
@@ -40,7 +39,7 @@ void xifrar_frase(char text[], char matrix[][7], char frase_xifrada[]){
                 if(i >= 1 && j >= 1){
                     if(text[k] == matrix[i][j]){
                         //printf("\n%c", matrix[i][j]);
-                        printf("%c%c", matrix[i][0], matrix[0][j]);
+                        //printf("%c%c", matrix[i][0], matrix[0][j]);
                         do{
                             if(l == 0){
                                 frase_xifrada[(o+l)] = matrix[i][0];
@@ -63,9 +62,9 @@ void xifrar_frase(char text[], char matrix[][7], char frase_xifrada[]){
 
 }
 
-void desxifrar_frase(char textXifrat[], char matrix[][7]){
-    int n, m, z = 0;
-    bool triggerRow, triggerColumn;
+void desxifrar_frase(char textXifrat[], char matrix[][7], char frase_desxifrada[]){
+    int n, m, z = 0, u = 1, j = 0, lenghtOfDecript = 0;
+    bool triggerRow, triggerColumn, secondValue = false;
     while (z < obtindreMida(textXifrat)){
         triggerRow = false;
         triggerColumn = false;
@@ -93,22 +92,40 @@ void desxifrar_frase(char textXifrat[], char matrix[][7]){
         }while(!triggerColumn);
 
         //demo decript
-        printf("%c", matrix[n][m]);
+        //printf("\n%c", matrix[n][m]);
+
+        do{
+            //esperaré a la segona comprovació ja que no he de fer tantes repeticions
+            if(u == 0){
+                secondValue = true;
+                u++;
+            }else{
+                frase_desxifrada[j] = matrix[n][m];
+                secondValue = false;
+                u = 0;
+                j++;
+            } 
+        }while(!secondValue && u == 0);
     }
+    frase_desxifrada[j] = '\0';
 }
 
 
 
 int main() {
     char message[] = "CONTINUEM PLA2";
-    char encryptedMessage[] = "NFRFRIMIOFRIMFNARXAFRROTNXAAASDASDSADAD";
+    char encryptedMessage[] = "NFRFRIMIOFRIMFNARXAFRROTNXAA";
     char matrix[7][7] = {};
+    //un text encriptat d'aquesta forma equival a el doble de la seva mida, ja que per cada lletra crea 2 caracters
     char frase_xifrada[(obtindreMida(message)*2)];
+    char frase_desxifrada[(obtindreMida(message))];
 
     construir_matriu_xifrat(matrix);
     xifrar_frase(message, matrix, frase_xifrada);
-    
     printf("\n%s", frase_xifrada);
+    desxifrar_frase(frase_xifrada, matrix, frase_desxifrada);
+    //printf("\n%s", frase_xifrada);
+    printf("\n%s", frase_desxifrada);
    
 
     return 0;
